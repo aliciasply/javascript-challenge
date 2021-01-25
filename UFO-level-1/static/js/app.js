80fa60a712d834a66ce190e03b7cab3283b34337
Alicia Ly <AliciaLy@alicias-mbp-2.lan>
2021-01-25 00:47:34 -0800

    added UFO js file

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,61 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Viewing the data from the console
+// console.log(tableData);
+
+// Creating references 
+var tablehead = d3.select("thead");
+var tablebody = d3.select("tbody");
+
+var inputElement = d3.select("#datetime");
+
+// Creating a function to show the table 
+function infoTable() {
+    tableData.map(data => {
+
+        // Make new row
+        var row = tablebody.append("tr");
+    
+        // example from day3 ex3
+        // Append each row with data
+        // using td instead of tr b/c <td> element defines a cell of a table that contains data. Displa table cell mode
+        row.append("td").text(data.datetime);
+        row.append("td").text(data.city);
+        row.append("td").text(data.state);
+        row.append("td").text(data.country);
+        row.append("td").text(data.shape);
+        row.append("td").text(data.durationMinutes);
+        row.append("td").text(data.comments);
+    });
+};
+
+// Displaying the table
+infoTable();
+
+// Create event handlers
+var button = d3.select("#filter-btn");
+
+button.on("click", handleClick);
+
+function handleClick() {
+    console.log("A button was clicked!");
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    console.log(inputValue);
+   
+    var filteredData = tableData.filter(data => data.datetime === inputValue);
+  
+    console.log(filteredData);
+
+};
+button.on("click", handleClick);
+
